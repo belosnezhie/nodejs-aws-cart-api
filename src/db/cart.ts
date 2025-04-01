203f0f1a9ee163305644d312e771207ac6426270
Maria Treier <mariatraier@gmail.com>
2025-04-01 23:28:00 +0200

    feat: create cart typeorm

---
+++ src/db/cart.ts
@@ -0,0 +1,38 @@
+import {
+  Entity,
+  Column,
+  PrimaryColumn,
+  CreateDateColumn,
+  OneToMany,
+} from 'typeorm';
+import { CartItem } from './cartItem';
+
+enum CartStatuses {
+  OPEN = 'OPEN',
+  ORDERED = 'ORDERED',
+}
+
+@Entity()
+export class Cart {
+  @PrimaryColumn('uuid')
+  id: string;
+
+  @Column('uuid')
+  user_id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @CreateDateColumn()
+  updated_at: Date;
+
+  @Column({
+    type: 'enum',
+    enum: CartStatuses,
+    default: CartStatuses.OPEN,
+  })
+  status: CartStatuses;
+
+  @OneToMany(() => CartItem, (item: CartItem) => item.cart)
+  items: CartItem[];
+}
