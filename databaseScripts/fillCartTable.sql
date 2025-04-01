ba2dd77bdd3ff48b3be0d49a409c8321658bb4d9
Maria Treier <mariatraier@gmail.com>
2025-04-01 22:20:37 +0200

    feat: create fill tables scripts

---
+++ databaseScripts/fillCartTable.sql
@@ -0,0 +1,25 @@
+INSERT INTO carts (id, user_id, created_at, updated_at, status)
+VALUES ('f4759375-f690-4b29-a1e4-8dc2ce5feb99',
+        '9f7541ed-92ce-4934-86b7-8ca48abd8ae3',
+        CURRENT_DATE,
+        CURRENT_DATE,
+        'OPEN'
+        ),
+        ('87078d3a-47af-4361-b76d-d8bffd37e8dc',
+        'd3fc83f0-4248-4161-bd4f-da0510244433',
+        CURRENT_DATE,
+        CURRENT_DATE,
+        'OPEN'
+        ),
+        ('be679f5c-e460-4671-b648-f6604e5aa6d6',
+        '59fb5d0a-f820-49f6-b608-4f06f9fad90b',
+        CURRENT_DATE,
+        CURRENT_DATE,
+        'OPEN'
+        ),
+        ('a3ba686a-efe1-4925-9f5f-8b88d287f9b3',
+        'fff6202b-1a4c-49d3-9646-f6777bc6c2a9',
+        CURRENT_DATE,
+        CURRENT_DATE,
+        'OPEN'
+        );
